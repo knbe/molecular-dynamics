e1be04a326ae5f55dd6800a725999dbac20c7f43
K A <nrv2209@protonmail.com>
2023-03-06 17:15:41 -0500

    bogged down

--- lib.jl
+++ lib.jl
@@ -7,9 +7,9 @@ using Statistics
 using Plots
 using Vectors
 
-@static if @isdefined(render)
-	using GLMakie
-end
+#@static if @isdefined(render)
+#	using GLMakie
+#end
 
 dt = 0.001
 sampleInterval = 100
@@ -342,165 +342,45 @@ function std_energy(sys::ParticleSystem2D)
 	return std(sys.energyPoints)
 end
 
+# GRAPHS
+################################################################################
+
+function plot_positions(sys::ParticleSystem2D)
+	N = sys.numParticles
+	plot()
+	scatter!(sys.x[1:2:2N], sys.x[2:2:2N])
+	xlabel!("x")
+	ylabel!("y")
+end
+
+function plot_trajectories(sys::ParticleSystem2D, number::Int64=1)
+	N = sys.numParticles
+	#plot()
+end
+
+function plot_temperature(sys::ParticleSystem2D)
+	plot()
+	plot!(sys.tPoints, sys.tempPoints)
+	xlabel!("t")
+	ylabel!("T")
+end
+
+function plot_energy(sys::ParticleSystem2D)
+	plot()
+	plot!(sys.sampleTimePoints, sys.energyPoints)
+	xlabel!("t")
+	ylabel!("energy")
+end
+
+function velocity_histogram(sys::ParticleSystem2D)
+	plot()
+	histogram!(sys.vPoints, normalize=:pdf)
+	xlabel!("velocity")
+	ylabel!("probability")
+end
+
 # RUN
 ################################################################################
 
 sys = ParticleSystem2D(4)
 lennard_jones_force(sys)
-
-#function powerLawForce(s::System)
-#end
-#
-## TIME EVOLUTION
-#
-#function verletStep!(s::System)
-#	a = force(s)
-#	s.x .+= s.v .* dt .+ 0.5 * dt^2 .* a
-#	s.x = s.x .% s.L
-#	s.v .+= 0.5 * dt .* (a + force(s))
-#end
-#
-#function evolve!(s::System, time::Float64=10.0)
-#	numSteps = Int64(time/dt)
-#
-#	for t in 1:numSteps
-#		verletStep!(s)
-#		zeroTotalMomentum!(s)
-#
-#		s.t += dt
-#		push!(s.tPoints, s.t)
-#
-#		if (t % sampleInterval == 0)
-#			push!(s.sampleTimePoints, s.t)
-#			#push!(s.energyPoints, energy(s))
-#			push!(s.xPoints, s.x)
-#			push!(s.vPoints, s.v)
-#		end
-#
-#		T = temperature(s)
-#		s.steps += 1
-#		push!(s.tempPoints, T)
-#		s.tempAccum += T
-#		s.sqTempAccum += T^2
-#	end
-#end
-#
-#function reverseTime(s::System)
-#end
-#
-#function cool(s::System, time::Float64=1.0)
-#end
-#
-## INITIALISATION
-#
-#function randomPositions(s::System)
-#end
-#
-#function triangularLatticePositions(s::System)
-#end
-#
-#function rectangularLatticePositions!(s::System)
-#	nx = Int64(sqrt(s.N))
-#	ny = nx
-#	dx = s.L / nx
-#	dy = s.L / ny
-#
-#	n = 1
-#	for i in 1:nx
-#		for j in 1:ny
-#			s.x[n] = (i - 0.5) * dx
-#			s.x[n+s.N] = (j - 0.5) * dy
-#			n += 1
-#		end
-#	end
-#end
-#
-#function zeroTotalMomentum!(s::System)
-#	vx = s.v[1:s.N]
-#	vy = s.v[(s.N + 1):(2 * s.N)]
-#
-#	vx .-= mean(vx)
-#	vy .-= mean(vy)
-#
-#	s.v[1:s.N] = vx
-#	s.v[(s.N + 1):(2 * s.N)] = vy
-#
-#end
-#
-#function randomVelocities!(s::System)
-#	s.v = rand(2 * s.N) .- 0.5
-#	zeroTotalMomentum!(s)
-#	T = temperature(s)
-#	rescale = sqrt(s.initTemp / T)
-#	s.v .*= rescale
-#end
-#
-## MEASUREMENTS
-#
-#function kineticEnergy(s::System)
-#	return 0.5 * mag_sq(s.v)
-#end
-#
-#function potentialEnergy(s::System)
-#end
-#
-#function lennardJonesPE(s::System)
-#end
-#
-#function energy(s::System)
-#end
-#
-#function temperature(s::System)
-#	return kineticEnergy(s) / s.N
-#end
-#
-## STATISTICS
-#
-#function resetStatistics(s::System)
-#end
-#
-#function meanTemperature(s::System)
-#end
-#
-#function meanSqTemperature(s::System)
-#end
-#
-#function meanPressure(s::System)
-#end
-#
-#function heatCapacity(s::System)
-#end
-#
-#function meanEnergy(s::System)
-#end
-#
-#function stdEnergy(s::System)
-#end
-#
-## PRINT RESULTS
-#
-#function results(s::System)
-#	println(
-#		  "  time:			", s.t, 
-#		#"\n  total energy:		", energy(s), 
-#		"\n  temperature:		", temperature(s),
-#		)
-#end
-#
-#
-#function plot_positions(s::System)
-#	N = s.N
-#	scatter(s.x[1:N], s.x[(N+1):2N])
-#end
-#
-#lennardJones = 1
-#
-#s = System(64, 10.0, 1.0)
-#
-#rectangularLatticePositions!(s)
-#randomVelocities!(s)
-#results(s)
-#
-#evolve!(s, 10.0)
-#results(s)
-#plot_positions(s)
